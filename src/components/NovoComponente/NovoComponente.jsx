d706bd464cd6dd247d34ea62bb1aa655ddd02f5d
LUCAS ALVES CASTELA PEREIRA <202402705989@alunos.ibmec.edu.br>
2024-10-10 14:04:44 -0300

    modificações

---
+++ src/components/NovoComponente/NovoComponente.jsx
@@ -0,0 +1,12 @@
+const NovoComponente = () =>(
+    <div>
+        <h2>Conteúdo do componente</h2>
+        <p>Um paragrafo</p>
+    </div>
+
+
+
+
+);
+
+export default NovoComponente;
---
+++ src/pages/Nova.jsx
@@ -0,0 +1,18 @@
+import NovoComponente from "../components/NovoComponente/NovoComponente";
+import Base from "./Base";
+
+
+const Nova = () =>{
+return(
+<>
+<Base>
+<h1> Nova pagina </h1> 
+<p>"oi"</p>
+<NovoComponente/>
+</Base>
+</>
+);
+
+}
+
+export default Nova;
